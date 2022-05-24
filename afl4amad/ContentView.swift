36fc8dae5484be3d24f57f6c86f32197d4274593
AverillSaladin03 <76471341+AverillSaladin03@users.noreply.github.com>
2022-05-24 08:46:03 +0700

    Initial Commit

---
+++ afl4amad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  afl4amad
+//
+//  Created by Lenny Indria on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ afl4amad/afl4amadApp.swift
@@ -0,0 +1,17 @@
+//
+//  afl4amadApp.swift
+//  afl4amad
+//
+//  Created by Lenny Indria on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct afl4amadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
